34203cd8d0876bda2a3a16d69362882ff121df11
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-04-29 10:18:37 +0200

    Sync af samlet program

--- MicrowaveOvenUnitTested/MicrowaveOven.Test.Integration/IT8_Door_UserInterface.cs
+++ MicrowaveOvenUnitTested/MicrowaveOven.Test.Integration/IT8_Door_UserInterface.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace MicrowaveOven.Test.Integration
 {
